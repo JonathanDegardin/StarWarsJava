be25f95cb3645aa8e43eeb6ba7157deb12016b6c
JonathanDegardin <74179678+JonathanDegardin@users.noreply.github.com>
2021-04-22 14:38:17 +0200

    Add files via upload

--- appClientModule/Main.java
+++ appClientModule/Main.java
@@ -1,14 +1,31 @@
-import StarWars;
+import java.util.Scanner;
+
+import StarWars.*;
 public class Main {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Film unFilm = new Film("LeFilm1",1998,4,154,185);
+		Film unSecondFilm = new Film("LeFilm2",2012,6,256,450);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Veuillez saisir le titre du film : ");
+		String titre = sc.nextLine();
+		System.out.println("Veuillez saisir l'année de sortie du film : ");
+		String annee = sc.nextLine();
+		int anneeSortie = Integer.parseInt(annee);
+		System.out.println("Veuillez saisir le nombre d'épisode de la saga : ");
+		String episode = sc.nextLine();
+		int episodeFilm = Integer.parseInt(episode);
+		System.out.println("Veuillez saisir le cout du film : ");
+		String cout = sc.nextLine();
+		int coutFilm = Integer.parseInt(cout);
+		System.out.println("Veuillez saisir la recette du film : ");
+		String recette = sc.nextLine();
+		int recetteFilm = Integer.parseInt(recette);
+		Film unDernierFilm = new Film(titre,anneeSortie,episodeFilm,coutFilm,recetteFilm);
+		System.out.println(unFilm.ToString());
+		System.out.println(unSecondFilm.ToString());
+		System.out.println(unDernierFilm.ToString());
 	}
 
-	/* (non-Java-doc)
-	 * @see java.lang.Object#Object()
-	 */
-	public Main() {
-		super();
-	}
+
 
 }
